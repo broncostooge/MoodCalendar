8b16b602d7a61b25695814f9e05f696dff9963b5
Stephan Jones <sjones.csuf@gmail.com>
2019-01-17 22:29:58 -0800

    Updates

---
+++ precache-manifest.12d28edb5f1054e4fd8c3364afe1f7d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cef6f33c42a1d023e4d",
+    "url": "/MoodCalendar/static/css/main.e1775846.chunk.css"
+  },
+  {
+    "revision": "3cef6f33c42a1d023e4d",
+    "url": "/MoodCalendar/static/js/main.3cef6f33.chunk.js"
+  },
+  {
+    "revision": "c5c6f401e305621ff949",
+    "url": "/MoodCalendar/static/js/1.c5c6f401.chunk.js"
+  },
+  {
+    "revision": "82d23efccbd3a228d582",
+    "url": "/MoodCalendar/static/js/runtime~main.82d23efc.js"
+  },
+  {
+    "revision": "7da07c4ea0c0c60d91a300702c53d8e7",
+    "url": "/MoodCalendar/index.html"
+  }
+];
